8b3e3cb4273b8614b01282212831d47053c8ad3a
speedshi <speedshi@hotmail.com>
2021-09-08 16:03:42 +0200

    update

--- run_multiple.sh
+++ run_multiple.sh
@@ -2,7 +2,7 @@
 
 for FOLDER in '20181201' '20181202' '20181203' '20181204' '20181205' 
 do
-    FPATH="../data/seismic_data_raw/${FOLDER}"
+    FPATH="../data/seismic_data_raw/seismic_raw_${FOLDER}"
     echo "Proceed to data: ${FPATH}"
     python run_MALMI_multiple.py "${FPATH}"
 
