bd8d21cb04d149543a24be6a51a74c0874de22b4
speedshi <speedshi@hotmail.com>
2022-01-15 21:13:42 +0100

    update run_MALMI

--- run_MALMI.py
+++ run_MALMI.py
@@ -10,7 +10,6 @@ Created on Tue Aug 10 21:13:03 2021
 import sys
 sys.path.append('/home/peidong/xresearch/code/MALMI/src')  # user need to change the path here!
 from main import MALMI
-import gc
 import datetime
 import os
 
@@ -40,7 +39,6 @@ control['n_processor'] = 6  # number of CPU processors for parallel processing
 tt = {}
 tt['vmodel'] = '../data/traveltime/velocity.txt'  # filename including path of the velocity model
 tt['dir'] = '../data/traveltime/tt_150m'  # path to travetime data directory
-tt['ftage'] = 'layer'  # traveltime data set filename tage as used for NonLinLoc
 
 
 # %% grid parameters used for migration========================================
@@ -55,23 +53,21 @@ grid['xNum'] = 250  # number of grid nodes in the X direction
 grid['yNum'] = 250  # number of grid nodes in the Y direction
 grid['zNum'] = 60  # number of grid nodes in the Z direction
 grid['dgrid'] = 0.4  # grid spacing in kilometers
+# grid = None
 
 
 # %% Initialize MALMI
 coseismiq = MALMI(seismic, tt, grid, control)
-gc.collect()
 
 
 # %% Format input data set
 coseismiq.format_ML_inputs()
-gc.collect()
 
 
 # %% Run ML models to get continuous phase probabilities
 input_MLmodel = '/home/peidong/xresearch/code/EQTransformer/ModelsAndSampleData/EqT_model.h5'  # path to a trained EQT model
 overlap = 0.8  # overlap rate of time window for generating probabilities. e.g. 0.6 means 60% of time window are overlapped
 coseismiq.generate_prob(input_MLmodel, overlap)
-gc.collect()
 
 
 # %% Detect locatable events from continuous phase probabilities
@@ -82,22 +78,18 @@ S_thrd = 0.05  # probability threshold for detecting S-phases/events from the ML
 nsta_thrd = 3  # minimal number of stations triggered during the specified event time period
 npha_thrd = 6  # minimal number of phases triggered during the specified event time period
 coseismiq.event_detect_ouput(twind_srch, twlex, P_thrd, S_thrd, nsta_thrd, npha_thrd)
-gc.collect()
 
 
 # %% Migration location for each event
 probthrd = 0.01  # if maximum value of the input phase probabilites is larger than this threshold, the input trace will be normalized (to 1)
 coseismiq.migration(probthrd)
-gc.collect()
 
 
 # %% Generate waveform plots for each event
 coseismiq.rsprocess_view()
-gc.collect()
 
 
 # %% Delete the input continuous seismic data for ML models for saving disk space
 coseismiq.clear_interm()
-gc.collect()
 
 
