8b180eeba0fd292aaff0f52de380a84c39e47c43
speedshi <speedshi@hotmail.com>
2022-12-22 10:58:57 +0100

    add xpick file

---
+++ src/xpick.py
@@ -0,0 +1,26 @@
+'''
+Functions for Seismic Phase Picking.
+
+Autor: Peidong Shi
+Contact: speedshi@hotmail.com  or  peidong.shi@sed.ethz.ch
+Create time: 20221219
+'''
+
+
+
+
+
+def prob2pick(trace_prob, pick_thrd):
+    '''
+    trace_prob: Obspy trace object,
+                continuous phase probability of a particular phase (P or S) at a station.
+    pick_thrd: float,
+               picking threshold.
+    '''
+
+    picks = {}
+
+    return picks
+
+
+
